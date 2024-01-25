68f841e5f4cd0e42571536ca8c3aa35f63460bba
aleena-joseph-86 <115602866+aleena-joseph-86@users.noreply.github.com>
2024-01-25 21:21:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const card = document.querySelector(".card");
+
+
+function display(){
+    card.classList.add("cardcomein");
+}
+
+function disappear(){
+    card.classList.remove("cardcomein");
+}
